24dd2e5c7c3cdc16d4a9e3677571b771b5e085ff
Ahmed <aabdelmeged70@yahoo.com>
2016-12-31 12:30:53 +0200

    initial

---
+++ src/Dictionary_doc.cpp
@@ -0,0 +1,17 @@
+#include "Dictionary_doc.h"
+
+Dictionary_doc::Dictionary_doc()
+{
+    dictionary[1]="Dr. Ahmed Saber";                dictionary[8]="Dr. Sara Khalil";
+    dictionary[2]="Dr. Ahmed Mohamed";              dictionary[9]="Dr. Mohamed Seliman";
+    dictionary[3]="Dr. Mohamed Gomaa";              dictionary[11]="Dr. Saleh Ahmed";
+    dictionary[4]="Dr. Fathy Samir";                dictionary[12]="Dr. Abdelrahman kamel";
+    dictionary[5]="Dr. Dalia Saber";                dictionary[13]="Dr. Amr Tawfik";
+    dictionary[6]="Dr. Zaki Ismail";                dictionary[14]="Dr. Omar Salah";
+    dictionary[7]="Dr. Rawan Mohsen";               dictionary[15]="Dr. Mohamed Hesham";
+}
+
+Dictionary_doc::~Dictionary_doc()
+{
+
+}
---
+++ src/Search.cpp
@@ -0,0 +1,5 @@
+#include "Search.h"
+/*void Search:: print ()
+{
+
+}*/
